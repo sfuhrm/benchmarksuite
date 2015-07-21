a6c49ab202bd48be657915539a5f9579348ee426
Stephan Fuhrmann <s_fuhrm@gmx.de>
2015-07-21 17:37:18 +0200

    Abort criteria for warmup

--- src/main/java/de/tynne/streamperformance/BenchmarkRunner.java
+++ src/main/java/de/tynne/streamperformance/BenchmarkRunner.java
@@ -9,7 +9,8 @@ public class BenchmarkRunner implements Runnable {
     @Getter
     private final List<Benchmark> benchmarks;
 
-    private final int batchSize = 4;
+    private final int BATCH_SIZE = 4;
+    private final int ITERATIONS_MAX = 100;
         
     public BenchmarkRunner(Collection<Benchmark> in) {
         this.benchmarks = new ArrayList<>(in);
@@ -18,7 +19,6 @@ public class BenchmarkRunner implements Runnable {
     @Override
     public void run() {
         int progress = 0;
-        int warmups = 0;
         int total = benchmarks.size();
         
         for (Benchmark b : benchmarks) {
@@ -31,7 +31,7 @@ public class BenchmarkRunner implements Runnable {
                 b.reset();
                 
                 // warm up the jit
-                for (int i=0; i < batchSize; i++) {
+                for (int i=0; i < BATCH_SIZE; i++) {
                     b.run();                
                 }
                 
@@ -39,12 +39,11 @@ public class BenchmarkRunner implements Runnable {
                 double max = b.getNanoTimes().max().getAsDouble();
                 double diff = Math.abs(max - min);
                 double dev = (double)diff / (double)max;
-                System.out.printf("Dev: %g\n", dev);
+                System.out.printf("%d. Dev: %g\n", iterations, dev);
                 warm = dev  < 0.01;
                 iterations++;
-            } while (!warm);
+            } while (!warm && iterations < ITERATIONS_MAX);
             System.err.println("Warm after "+iterations+" iterations.");
-            warmups += Math.max(0, iterations - 1);
             progress++;
         }
     }
