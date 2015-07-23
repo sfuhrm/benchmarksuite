a1be766c5a4056d4992e649919f4bbf9a0338bbf
Stephan Fuhrmann <s_fuhrm@gmx.de>
2015-07-23 15:42:57 +0200

    Added the standard deviation

--- src/main/java/de/tynne/streamperformance/Main.java
+++ src/main/java/de/tynne/streamperformance/Main.java
@@ -8,6 +8,7 @@ import java.nio.charset.Charset;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
@@ -16,6 +17,11 @@ import org.apache.commons.csv.CSVPrinter;
  * @author fury
  */
 public class Main {
+
+    private static double standardDeviationOf(DoubleStream doubleStream, double average) {
+        double sum = doubleStream.map(x -> Math.pow(x - average, 2.)).sum();
+        return Math.sqrt(sum);
+    }
     
     public static void main(String[] cmdLine) throws FileNotFoundException, IOException {  
         Args args = Args.parse(cmdLine);
@@ -24,11 +30,10 @@ public class Main {
         }
         
         BenchmarkProducer benchmarkProducer = new MinimumJ8Benchmarks();
-        
 
         BackupHelper.backupIfNeeded(args.getOutput());
         
-        final CSVFormat format = CSVFormat.EXCEL.withDelimiter(';').withHeader("#", "ID", "Name", "Min [ns]", "Avg [ns]", "Median [ns]", "Max [ns]", "Chart Pos", "Best Increase [%]");
+        final CSVFormat format = CSVFormat.EXCEL.withDelimiter(';').withHeader("#", "ID", "Name", "Min [ns]", "Avg [ns]", "Median [ns]", "Max [ns]", "Std Dev [ns]", "Chart Pos", "Best Increase [%]");
         try (CSVPrinter printer = new CSVPrinter(new OutputStreamWriter(new FileOutputStream(args.getOutput()), Charset.forName(args.getCharset())), format)) {
             List<Benchmark> benchmarks = benchmarkProducer.get();
             List<Benchmark> matching = benchmarks.stream().filter(b -> args.getExecute().matcher(b.getId()).matches()).collect(Collectors.toList());
@@ -49,6 +54,7 @@ public class Main {
                             printer.print(doubleSummaryStatistics.getAverage());
                             printer.print(b.getMedian().getAsDouble());
                             printer.print(doubleSummaryStatistics.getMax());
+                            printer.print(standardDeviationOf(b.getNanoTimes(), doubleSummaryStatistics.getAverage()));
                             printer.print(chart.getChart().get(b).chartPosition);
                             double bestAvg = chart.getStats().get(chart.getPerformanceChart().get(0)).getAverage();
                             double thisAvg = doubleSummaryStatistics.getAverage();
