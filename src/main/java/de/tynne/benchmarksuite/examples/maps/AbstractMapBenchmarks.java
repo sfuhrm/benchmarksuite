87d04001ccdbb0334434446811796f1282a9dca9
Stephan Fuhrmann <s_fuhrm@gmx.de>
2015-08-27 00:56:00 +0200

    Removed import

--- src/main/java/de/tynne/benchmarksuite/examples/maps/AbstractMapBenchmarks.java
+++ src/main/java/de/tynne/benchmarksuite/examples/maps/AbstractMapBenchmarks.java
@@ -4,7 +4,6 @@ import de.tynne.benchmarksuite.Benchmark;
 import de.tynne.benchmarksuite.BenchmarkProducer;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.LinkedHashMap;
