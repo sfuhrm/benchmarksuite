7c1a87f0bd0d15bd1e7ea9711b2acdcad9867317
Stephan Fuhrmann <s_fuhrm@gmx.de>
2017-01-14 16:49:30 +0100

    Imports

--- src/test/java/de/sfuhrm/benchmarksuite/IDGeneratorTest.java
+++ src/test/java/de/sfuhrm/benchmarksuite/IDGeneratorTest.java
@@ -15,8 +15,6 @@
  */
 package de.sfuhrm.benchmarksuite;
 
-import java.util.Arrays;
-import java.util.Collections;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
